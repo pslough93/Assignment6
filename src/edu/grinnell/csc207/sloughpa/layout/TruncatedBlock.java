4979c1c9d622a493511e290c6dc286a293ee79a8
Patrick Slough <sloughpa@grinnell.edu>
2014-09-30 11:01:00 -0500

    Formatted correctly

--- src/edu/grinnell/csc207/sloughpa/layout/TruncatedBlock.java
+++ src/edu/grinnell/csc207/sloughpa/layout/TruncatedBlock.java
@@ -1,46 +1,52 @@
 package edu.grinnell.csc207.sloughpa.layout;
 
-public class TruncatedBlock implements TextBlock{
-	
-	TextBlock contents;
-	int width;
-/**
- * Create a new truncated block of the specified width.
- */
-public TruncatedBlock(TextBlock tb, int width)
+public class TruncatedBlock
+    implements TextBlock
 {
-	contents = tb;
-	this.width = width;
-} // TruncatedBlock(TextBlock, int)
 
-/**
- * Get the ith row of the block.
- * @throws Exception 
- */
-public String row(int i) throws Exception
-{
-  if(i >= 0 && i < contents.height()){
-	  return contents.row(i).substring(0,width);
-  }
-  else{
-	  throw new Exception("Invalid row " + i);
-  }
-} // row(int)
+  TextBlock contents;
+  int width;
 
-/**
- * Determine how many rows are in the block.
- */
-public int height()
-{
-  return contents.height();
-} // height()
+  /**
+  * Create a new truncated block of the specified width.
+  */
+  public TruncatedBlock(TextBlock tb, int width)
+  {
+    contents = tb;
+    this.width = width;
+  } // TruncatedBlock(TextBlock, int)
 
-/**
- * Determine how many columns are in the block.
- */
-public int width()
-{
-  return contents.width();
-} // width()
+  /**
+   * Get the ith row of the block.
+   * @throws Exception 
+   */
+  public String row(int i)
+    throws Exception
+  {
+    if (i >= 0 && i < contents.height())
+      {
+        return contents.row(i).substring(0, width);
+      }
+    else
+      {
+        throw new Exception("Invalid row " + i);
+      }
+  } // row(int)
+
+  /**
+   * Determine how many rows are in the block.
+   */
+  public int height()
+  {
+    return contents.height();
+  } // height()
+
+  /**
+   * Determine how many columns are in the block.
+   */
+  public int width()
+  {
+    return contents.width();
+  } // width()
 } // class TruncatedBlock
 
